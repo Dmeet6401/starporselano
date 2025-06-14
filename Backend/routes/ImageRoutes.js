d0ed9bb6e89f0ba220c7b3c54482dbc13c139886
Dmeet6401 <desai.meet06@gmail.com>
2025-06-15 01:18:11 +0530

    k issue

--- Backend/routes/ImageRoutes.js
+++ Backend/routes/ImageRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { uploadToImageKit,uploadPhoto } = require('../services/imageKitService');
+const { uploadToImageKit,uploadPhoto } = require('../services/imagekitService');
 
 // Route for uploading 
 router.post('/upload', uploadPhoto,uploadToImageKit);
